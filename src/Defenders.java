c6d117d855c9baa7b27ca4a3e09800651cf9b461
Mark Rofaeel <mark.rofaeel@gmail.com>
2020-12-27 22:39:15 +0200

    Changes to phase 2

--- src/Defenders.java
+++ src/Defenders.java
@@ -1,42 +1,48 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Defenders extends Squad
 { 
-	BufferedWriter myWriter;
-	FileWriter fr;
-	Users user = new Users();
-	Data myfile = new Data();
-	Scanner input = new Scanner(System.in);
-	public void UserDefenders(File file, String username) throws Exception
-	{     
-		String player;
-	    String x ="C:\\Users\\Mark Rofaeel\\eclipse-workspace\\Fantasy project\\Defenders.txt";
-	    while(Squad.defenders.size()<5)
-	    {
-	    	player = input.next();
-	    	String newname = check(player,x);
-	        checkPlayers(newname,x,username);
-	        if(Squad.PlayerNum<3)
-	        {
-	        	checkUserBudget(newname,x,username);
-		       	myfile.userFile(newname, username, x);
-		       	Squad.defenders.add(newname);
-		       	Squad.squad.add(newname);
+	 public void UserDefenders(String username) throws Exception
+	  {     
+			Scanner input= new Scanner(System.in);
+	       	String player ;
+	    	Path path = FileSystems.getDefault().getPath("Defenders.txt");
+			File defender = new File(path.toString());
+	       	while(Squad.defenders.size()<5)
+	       {
+	       		player = input.next();
+	       		String newname = check(player,defender);
+	       		checkPlayers(newname,defender,username);
+	       		if(Squad.PlayerNum<3)
+	       		{
+	       			checkUserBudget(newname,defender,username);
+		       		Squad.defenders.add(newname);
+		       		Squad.squad.add(newname);
+	       		}
+	       		else 
+	       		{
+	       			System.out.println("Error you entered the 3 players in the same club");
+	       			
+	       		}
+	       		
 	       	}
-	       	else 
-	       	{
-	       		System.out.println("You already have entered 3 players of the same team");	
-	       	}	
-	    }
-	}	 
+	    
+	  }	
+	 
 	public void add(String username) throws Exception
-	{
-		File fileUser = new File("C:\\Users\\Mark Rofaeel\\eclipse-workspace\\Fantasy project\\"+username+".txt");
+	{ 
+		Data myfile =new Data() ;
 		System.out.println("Add defenders:");
 		myfile.ReadToDefenders();
-        UserDefenders(fileUser,username);
+        UserDefenders(username);
 	}
 }
